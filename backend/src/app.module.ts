3f9991092c4214d61b0a28cc3652cc5dbdaaed26
geun-kim <geun.git.it@gmail.com>
2025-03-11 17:38:35 -0700

    revert

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,7 +17,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
     TypeOrmModule.forRootAsync({
       useFactory: async () => ({
         type: "postgres",
-        url: process.env.DATABASE_URL,
+        host: process.env.DATABASE_HOST || "localhost",
+        port: parseInt(process.env.DATABASE_PORT || "5432", 10),
+        username: process.env.DATABASE_USER || "postgres",
+        password: process.env.DATABASE_PASSWORD || "password",
+        database: process.env.DATABASE_NAME || "expenditure_analyzer",
         autoLoadEntities: true,
         synchronize: true,
       }),
